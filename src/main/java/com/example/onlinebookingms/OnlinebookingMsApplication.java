10b0c19ddeb1b95eb401ec2d235d6b58bf62f62e
Kalyan Vaidya <kalsv56@gmail.com>
2023-03-05 08:36:51 +0530

    onlinebooking

---
+++ src/main/java/com/example/onlinebookingms/OnlinebookingMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinebookingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookingMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinebookingms/OnlinebookingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinebookingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
